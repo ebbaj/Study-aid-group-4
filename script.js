7b4038dd333a621a2eab46e4d763834ded7bcf4d
ebbaj <ebba-jonsson@hotmail.com>
2020-10-20 18:40:52 +0200

    Made progressbar move with JS and started session form functionality

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-const group4 = 'Simply the best'
+const progressBar = document.querySelector('.progress-bar');
+const submitBtn = document.querySelector('.submit-session');
+const sessionAmount = document.getElementById('session-amount').value;
+const sessionLearnings = document.getElementById('session-learnings').value;
+
+let userGoal = 100;
+let userInput = 73;
+
+let sessionHistory = {
+    amountStudied: sessionAmount,
+    comment: sessionLearnings,
+}
+
+
+
+submitBtn.addEventListener('click', submitForm);
+
+function submitForm(){
+    console.log("Hello");
+    console.log(sessionHistory);
+}
+
+
+function moveProgressBar() {
+    let progress = userInput / userGoal;
+    let finalP = progress * 100;
+    progressBar.style.width = finalP + '%';
+    progressBar.innerHTML = `${finalP}%`;
+}
+
+moveProgressBar();
+
+
 
