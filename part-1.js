0e9a36f3ead66564cacd32288c603441c7b2c8a7
Sanvog <vogel.sanna@gmail.com>
2020-10-20 16:12:15 +0200

    Fix the function to dissapear

--- part-1.js
+++ part-1.js
@@ -1,16 +1,29 @@
 function showPages(){
     document.querySelector(".hide").style.display = "none";
     document.getElementById('hidden-pages').style.display = "block";
+    document.getElementById('hidden-hours').style.display = "none";
+    document.getElementById('hidden-exercises').style.display = "none";
 }
 function showHours(){
     document.querySelector(".hide").style.display = "none";
     document.getElementById('hidden-hours').style.display = "block";
+    document.getElementById('hidden-pages').style.display = "none";
+    document.getElementById('hidden-exercises').style.display = "none";
 }
 
 function showExercises(){
     document.querySelector(".hide").style.display = "none";
     document.getElementById('hidden-exercises').style.display = "block";
+    document.getElementById('hidden-pages').style.display = "none";
+    document.getElementById('hidden-hours').style.display = "none";
 }
+
+/* var removeHiddenPages=document.querySelector("#hidden-pages");
+    removeHiddenPages.checked = false;
+    var removeHiddenHours=document.querySelector("#hidden-hours");
+    removeHiddenHours.checked = false;
+    var removeHiddenExercises=document.querySelector("#hidden-exercises");
+	removeHiddenExercises.checked = false; */
 
 
 // function showInput(){
