53e57fe5850fe42d7aa9b901f7db70a753ad737c
Omid <omid.haqbin@gmail.com>
2020-10-20 15:37:29 +0200

    functions not working :)))))

--- part-1.js
+++ part-1.js
@@ -1,4 +1,22 @@
-
-function showDiv() {
-
+function showPages(){
+    document.querySelector(".hide").style.display = "none";
+    document.getElementById('hidden-pages').style.display = "block";
 }
+function showHours(){
+    document.querySelector(".hide").style.display = "none";
+    document.getElementById('hidden-hours').style.display = "block";
+}
+
+function showExercises(){
+    document.querySelector(".hide").style.display = "none";
+    document.getElementById('hidden-exercises').style.display = "block";
+}
+
+
+// function showInput(){
+//     if (document.getElementById("hours")) {
+//         document.getElementById("hidden-hours").style.display = "block";
+//     } else if (document.getElementById("pages")){
+//         document.getElementById("hidden-pages").style.display = "block";
+//     } 
+// }
