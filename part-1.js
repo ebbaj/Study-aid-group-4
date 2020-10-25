117e098a6006a13fcc59c2435db1549b447ac1ca
Omid <omid.haqbin@gmail.com>
2020-10-25 15:37:54 +0100

    Fixed showInputs()

--- part-1.js
+++ part-1.js
@@ -1,63 +1,63 @@
-const weekGoalTitle = document.getElementById('activity');
-const radioHours = document.getElementById('hours');
-const radioPages = document.getElementById('pages');
-const radioExercises = document.getElementById('exercises');
-const submitBtn = document.getElementById('submitButton');
 
 function showInputs(){
+
+    const radioHours = document.getElementById('hours');
+    const radioPages = document.getElementById('pages');
+    const radioExercises = document.getElementById('exercises');
+
     if (radioHours.checked){
-        document.getElementById('hidden-hours').style.display = "block"; 
+        document.getElementById('measurement-hours').required = true;
+        document.getElementById('hidden-hours').style.display = "block";
         document.getElementById('hidden-pages').style.display = "none";
         document.getElementById('hidden-exercises').style.display = "none";
     } else if (radioPages.checked) {
+        document.getElementById('measurement-pages').required = true;
         document.getElementById('hidden-pages').style.display = "block"; 
         document.getElementById('hidden-hours').style.display = "none";
         document.getElementById('hidden-exercises').style.display = "none";
     } else if (radioExercises.checked) {
+        document.getElementById('measurement-exercises').required = true;
         document.getElementById('hidden-exercises').style.display = "block"; 
         document.getElementById('hidden-pages').style.display = "none";
         document.getElementById('hidden-hours').style.display = "none";
     }
+
 }
-document.addEventListener('click', showInputs);
-
-
-
+document.getElementById('measureing-type').addEventListener('click', showInputs);
 
 
 
 let weekdays = document.getElementById("daysOptions");
-
-submitBtn.addEventListener('click', function(e) {
 let daysArray = [];
 
-weekdays.querySelectorAll("input").forEach(function (input){
-    if (input.type==='checkbox' && input.checked){
-        daysArray.push(input.value);
-    }
-})
-
-console.log(daysArray);
+function days() {
+    weekdays.querySelectorAll("input").forEach(function (input){
+        if (input.type ==='checkbox' && input.checked){
+            daysArray.push(input.value);
+        }
+    })
+    const submitBtn = document.getElementById('submitButton');
+    console.log(daysArray); 
+    submitBtn.addEventListener('click', days, function(ev){
+    ev.preventDefault()
 });
+}
 
-// function activityFun() {
-//     let activityName = document.getElementById('activity').value;
-//     console.log(activityName);
-// }
-// activityFun();
+function activityFun() {
+    let activityName = document.getElementById('activity').value;
+    console.log(activityName);
+}
+activityFun();
 
-// let mType = document.getElementById("measureing-type");
-// mType.querySelectorAll('input').forEach(function (input){
-//     if (input.type === 'radio' && input.checked){
-//         console.log(input.value);
-//     }
-// })
 
-// function mTypeAmount() {
-//     let mAmount = document.getElementById('measurement-goal').value;
-//     console.log(mAmount);
-// }
-// mTypeAmount();
+function mTypeAmount() {
+    const form = document.forms[0];
+    const selectElement = form.querySelector('input[name="amount"]');
+
+    let mAmount = selectElement.value;
+    console.log(mAmount);
+}
+mTypeAmount();
 
 
 
