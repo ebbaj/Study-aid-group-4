5acc4598e1205827c595e54d2a8b0309cbb6769e
Omid <omid.haqbin@gmail.com>
2020-10-25 08:55:37 +0100

    rewriting functions, cleaning up

--- part-1.js
+++ part-1.js
@@ -1,3 +1,8 @@
+const weekGoal = document.getElementById('activity');
+const radioHours = document.getElementById('hours');
+const radioPages = document.getElementById('pages');
+const radioExercises = document.getElementById('exercises');
+
 function showPages(){
     document.querySelector(".hide").style.display = "none";
     document.getElementById('hidden-pages').style.display = "block";
@@ -27,6 +32,60 @@ let numberOfDays;
 let weekdays; //array or if it is tricky to code then just show hours per day this week.
 
 
+<<<<<<< Updated upstream
+=======
+console.log(daysArray);
+
+function activityFun() {
+    let activityName = document.getElementById('activity').value;
+    console.log(activityName);
+}
+activityFun();
+
+let mType = document.getElementById("measureing-type");
+mType.querySelectorAll('input').forEach(function (input){
+    if (input.type === 'radio' && input.checked){
+        console.log(input.value);
+    }
+})
+
+function mTypeAmount() {
+    let mAmount = document.getElementById('measurement-goal').value;
+    console.log(mAmount);
+}
+mTypeAmount();
+
+
+
+});
+
+
+
+
+
+ //array or if it is tricky to code then just show hours per day this week.
+
+/* document.getElementById("weekdays").onsubmit=function() {
+    mon = parseInt(document.querySelector('input[name = "monday"]:checked').value);
+    tue = parseInt(document.querySelector('input[name = "tuesday"]:checked').value);
+    wed = parseInt(document.querySelector('input[name = "wednesday"]:checked').value);
+    thu = parseInt(document.querySelector('input[name = "thursday"]:checked').value);
+    fri = parseInt(document.querySelector('input[name = "friday"]:checked').value);
+    sat = parseInt(document.querySelector('input[name = "saturday"]:checked').value);
+    sun = parseInt(document.querySelector('input[name = "sunday"]:checked').value);
+
+result = mon + tue + wed + thu + fri + sat + sun;
+	
+ document.getElementById("grade").innerHTML = result;
+
+ if (result == 2) {result2 = "2 days"};
+
+ document.getElementById("grade2").innerHTML = result2; */
+
+// }
+	
+  
+>>>>>>> Stashed changes
 
 
 
