49437062770d6dd8674bf0ac2f1e52101c1ccf82
Omid <omid.haqbin@gmail.com>
2020-10-26 14:19:20 +0100

    I don't remember. I should've commited last night.

--- part-1.js
+++ part-1.js
@@ -1,9 +1,8 @@
+const radioHours = document.getElementById('hours');
+const radioPages = document.getElementById('pages');
+const radioExercises = document.getElementById('exercises');
 
 function showInputs(){
-
-    const radioHours = document.getElementById('hours');
-    const radioPages = document.getElementById('pages');
-    const radioExercises = document.getElementById('exercises');
 
     if (radioHours.checked){
         document.getElementById('measurement-hours').required = true;
@@ -15,7 +14,7 @@ function showInputs(){
         document.getElementById('hidden-pages').style.display = "block"; 
         document.getElementById('hidden-hours').style.display = "none";
         document.getElementById('hidden-exercises').style.display = "none";
-    } else if (radioExercises.checked) {
+    } else if (radioExercises.checked){
         document.getElementById('measurement-exercises').required = true;
         document.getElementById('hidden-exercises').style.display = "block"; 
         document.getElementById('hidden-pages').style.display = "none";
@@ -27,72 +26,43 @@ document.getElementById('measureing-type').addEventListener('click', showInputs)
 
 
 
-let weekdays = document.getElementById("daysOptions");
-let daysArray = [];
+const weekdays = document.getElementById("daysOptions");
+const submitBtn = document.getElementById('submitButton');
 
-function days() {
+
+submitBtn.addEventListener('click', function(e) {
+    let daysArray = [];
+
     weekdays.querySelectorAll("input").forEach(function (input){
-        if (input.type ==='checkbox' && input.checked){
+        if (input.type === 'checkbox' && input.checked){
             daysArray.push(input.value);
         }
     })
-    const submitBtn = document.getElementById('submitButton');
-    console.log(daysArray); 
-    submitBtn.addEventListener('click', days, function(ev){
-    ev.preventDefault()
-});
-}
 
-function activityFun() {
     let activityName = document.getElementById('activity').value;
     console.log(activityName);
-}
-activityFun();
 
+    function mType(){
+        if (radioHours.checked){
+            console.log(radioHours.value);
+            let hours = document.getElementById('measurement-hours');
+            console.log(hours.value);
+        } else if (radioPages.checked){
+            console.log(radioPages.value);
+            let pages = document.getElementById('measurement-pages');
+            console.log(pages.value);
+        } else if (radioExercises.checked){
+            console.log(radioExercises.value);
+            let exercises = document.getElementById('measurement-exercises');
+            console.log(exercises.value);
+        }
 
-function mTypeAmount() {
-    const form = document.forms[0];
-    const selectElement = form.querySelector('input[name="amount"]');
+    }
+    mType();
 
-    let mAmount = selectElement.value;
-    console.log(mAmount);
-}
-mTypeAmount();
+    console.log(daysArray);
 
-
-
-// });
-
-
-
-
-
- //array or if it is tricky to code then just show hours per day this week.
-
-/* document.getElementById("weekdays").onsubmit=function() {
-    mon = parseInt(document.querySelector('input[name = "monday"]:checked').value);
-    tue = parseInt(document.querySelector('input[name = "tuesday"]:checked').value);
-    wed = parseInt(document.querySelector('input[name = "wednesday"]:checked').value);
-    thu = parseInt(document.querySelector('input[name = "thursday"]:checked').value);
-    fri = parseInt(document.querySelector('input[name = "friday"]:checked').value);
-    sat = parseInt(document.querySelector('input[name = "saturday"]:checked').value);
-    sun = parseInt(document.querySelector('input[name = "sunday"]:checked').value);
-
-result = mon + tue + wed + thu + fri + sat + sun;
-	
- document.getElementById("grade").innerHTML = result;
-
- if (result == 2) {result2 = "2 days"};
-
- document.getElementById("grade2").innerHTML = result2; */
-
-// }
-	
-  
-
-
-
-
+});
 
 
 
