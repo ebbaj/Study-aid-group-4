d161adc959b642c3b7cf605e065244e8a9879ec5
ebbaj <ebba-jonsson@hotmail.com>
2020-10-28 00:14:58 +0100

    Started code for remove-buttons + storing progressbar and text fields in localStorage

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const submitBtn = document.querySelector(".submit-session");
 const progressBar = document.querySelector('.progress-bar');
 const list = document.querySelector(".inputList");
 
-//let loadInfo = JSON.parse(localStorage.getItem('data'));
-
 let loadInfo
 loadData()
 
@@ -31,16 +29,23 @@ function loadPreExisting() {
             inputItem.innerText = "Date: " + item.date + "\n" + "Activity: " + "\n" + "Amount: " + item.sessionAmount + "\n" + "Learning: " + item.sessionComment;
             list.appendChild(inputItem);
             inputItem.appendChild(removeButton);
+            
+            changeTextItems();
+            moveProgressBar();
+
+            removeButton.addEventListener('click', () => {            
+                inputItem.remove();
+            });
+            
+
         }
     }
 }
+
 let sessionInfo;
 
-//Variables - userGoal and userInput are temporary variables to test the progress bar
-let userGoal = 100;
-let userInput = 60; /* 55-58 gets decimals, fix this bug */
-
 submitBtn.addEventListener("click", storeSession);
+
 //Functions
 function storeSession(event){ 
     event.preventDefault(); 
@@ -48,7 +53,10 @@ function storeSession(event){
     sessionInfo = {
     'date': new Date().toISOString().slice(0, 10),
     'sessionAmount': document.getElementById('session-amount').value,
-    'sessionComment': document.getElementById('session-learnings').value
+    'sessionComment': document.getElementById('session-learnings').value,
+    'weeklyGoal': document.querySelector('.weeklyGoal').innerHTML,
+    'dailyGoal': document.querySelector('.todaysGoal').innerHTML,
+    'progressBar': document.querySelector('.progress-bar'),
     }
 
     loadInfo.push(sessionInfo);
@@ -60,17 +68,35 @@ function storeSession(event){
     inputItem.innerText = "Date: " + sessionInfo.date + "\n" + "Activity: " + "\n" + "Amount: " + sessionInfo.sessionAmount + "\n" + "Learning: " + sessionInfo.sessionComment;
     list.appendChild(inputItem);
     inputItem.appendChild(removeButton);
+
+    changeTextItems();
+    moveProgressBar();
+    
+
+    removeButton.addEventListener('click', () => {
+        inputItem.remove();
+    }); 
 }
 
 function moveProgressBar() {
+    //Variables - userGoal and userInput are temporary variables to test the progress bar
+    let userGoal = 100;
+    let userInput = 80; /* 55-58 gets decimals, fix this bug */
+
     let progress = userInput / userGoal;
     let finalP = progress * 100;
     progressBar.style.width = finalP + '%';
     progressBar.innerHTML = `${finalP}%`;
 }
 
-moveProgressBar();
 
- 
+
+//Test to update innerHTML of text items - maybe make this into reCalc() function?
+function changeTextItems() {
+    let weeklyNumber = 50; //temporary values, will be based on userInput later on
+    let dailyNumber = 30;
+    document.querySelector(".weeklyGoal").innerHTML = "Finish " + weeklyNumber + " pages everyday";
+    document.querySelector(".todaysGoal").innerHTML = "Read " + dailyNumber + " pages";
+}
 
 
