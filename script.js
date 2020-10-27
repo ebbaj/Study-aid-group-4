934fe730d4b817e5c2f1fa4fa9106fe0c195400a
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2020-10-27 15:01:01 +0100

    Revert "Revert "Merge branch 'Part-2' of https://github.com/ebbaj/Study-aid-group-4 into Part-2""
    This reverts commit e4d623f89285be4b040586c12ba3266473ffcd96.

--- script.js
+++ script.js
@@ -1,40 +1,82 @@
 //Selectors
 const submitBtn = document.querySelector(".submit-session");
 const progressBar = document.querySelector('.progress-bar');
-const list = document.querySelector(".inputList")
+const list = document.querySelector(".inputList");
+
+let loadInfo = JSON.parse(localStorage.getItem('data'));
+loadPreExisting();
+function save() {
+    localStorage.setItem('data', JSON.stringify(loadInfo));
+}
+
+function loadPreExisting() {
+    for(let item of loadInfo) {
+        if(item) {
+            const inputItem = document.createElement("li");
+            const removeButton = document.createElement("button");
+            removeButton.innerText = "Remove";
+
+            inputItem.innerText = "Date: " + item.date + "\n" + "Activity: " + "\n" + "Amount: " + item.sessionAmount + "\n" + "Learning: " + item.sessionComment;
+            list.appendChild(inputItem);
+            inputItem.appendChild(removeButton);
+        }
+    }
+}
+let sessionInfo;
 
 //Variables - userGoal and userInput are temporary variables to test the progress bar.
 let userGoal = 100;
 let userInput = 60; /* 55-58 gets decimals, fix this bug */
 
-//Event Listeners
-submitBtn.addEventListener("click", storeSession);
+function doFirst() {   
+    display();
+}
 
+submitBtn.addEventListener("click", storeSession);
 //Functions
 function storeSession(event){ 
     event.preventDefault(); 
 
-    let sessionInfo = {
-        'sessionAmount': document.getElementById('session-amount').value,
-        'sessionComment': document.getElementById('session-learnings').value
+    sessionInfo = {
+    'date': new Date().toISOString().slice(0, 10),
+    'sessionAmount': document.getElementById('session-amount').value,
+    'sessionComment': document.getElementById('session-learnings').value
     }
 
+    loadInfo.push(sessionInfo);
+    save();
+    /*
     let old_data = JSON.parse(localStorage.getItem('data'));
     if(!(old_data instanceof Array))
     old_data = [old_data];
     old_data.push(sessionInfo);
     localStorage.setItem('data', JSON.stringify(old_data));
     console.log(old_data);
+    */
 
-    
     const inputItem = document.createElement("li");
     const removeButton = document.createElement("button");
     removeButton.innerText = "Remove";
-    inputItem.innerText = "Date: " + "\n" + "Activity: " + "\n" + "Amount: " + sessionInfo.sessionAmount + "\n" + "Learning: " + sessionInfo.sessionComment;
+
+    inputItem.innerText = "Date: " + sessionInfo.date + "\n" + "Activity: " + "\n" + "Amount: " + sessionInfo.sessionAmount + "\n" + "Learning: " + sessionInfo.sessionComment;
     list.appendChild(inputItem);
     inputItem.appendChild(removeButton);
-   
 }
+/*
+function display() {
+    if(localStorage.length != 0){
+        listSession();
+    }
+}
+
+
+window.addEventListener('load', doFirst);
+
+*/
+
+
+
+
 
 
 function moveProgressBar() {
