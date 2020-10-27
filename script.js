aaafdc6cf891c888812b453effae718c555b39b7
ebbaj <ebba-jonsson@hotmail.com>
2020-10-27 10:26:18 +0100

    Made localStorage into array of objects and made user input appear on webpage

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 //Selectors
 const submitBtn = document.querySelector(".submit-session");
 const progressBar = document.querySelector('.progress-bar');
-//let submitBtn = document.getElementById('submit-session');
-let sessionAmount = document.getElementById('session-amount');
-let sessionLearnings = document.getElementById('session-learnings');
+const list = document.querySelector(".inputList")
 
 //Variables - userGoal and userInput are temporary variables to test the progress bar.
 let userGoal = 100;
 let userInput = 60; /* 55-58 gets decimals, fix this bug */
-
-let nbrKey = "Number";
-let cmtKey = "Comment";
-
 
 //Event Listeners
 submitBtn.addEventListener("click", storeSession);
@@ -19,38 +13,29 @@ submitBtn.addEventListener("click", storeSession);
 //Functions
 function storeSession(event){ 
     event.preventDefault(); 
-    
-    let sValue = sessionAmount.value;
-    let sComment = sessionLearnings.value;
 
-    let i = localStorage.length;
-
-    if(localStorage.length == 0) {
-        //let i = 0;
-        i++;
-        nbrKey = "Number";
-        cmtKey = "Comment";
-
-        nbrKey = nbrKey + i;
-        cmtKey = cmtKey + i;
-
-        localStorage.setItem(nbrKey, sValue);
-        localStorage.setItem(cmtKey, sComment);
-        console.log(localStorage);
-    } else {
-       
-        i++;
-        nbrKey = "Number";
-        cmtKey = "Comment";
-
-        nbrKey = nbrKey + i;
-        cmtKey = cmtKey + i;
-
-        localStorage.setItem(nbrKey, sValue);
-        localStorage.setItem(cmtKey, sComment);
-        console.log(localStorage);
+    let sessionInfo = {
+        'sessionAmount': document.getElementById('session-amount').value,
+        'sessionComment': document.getElementById('session-learnings').value
     }
+
+    let old_data = JSON.parse(localStorage.getItem('data'));
+    if(!(old_data instanceof Array))
+    old_data = [old_data];
+    old_data.push(sessionInfo);
+    localStorage.setItem('data', JSON.stringify(old_data));
+    console.log(old_data);
+
+    
+    const inputItem = document.createElement("li");
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "Remove";
+    inputItem.innerText = "Date: " + "\n" + "Activity: " + "\n" + "Amount: " + sessionInfo.sessionAmount + "\n" + "Learning: " + sessionInfo.sessionComment;
+    list.appendChild(inputItem);
+    inputItem.appendChild(removeButton);
+   
 }
+
 
 function moveProgressBar() {
     let progress = userInput / userGoal;
