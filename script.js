71ba2ebe7e04a74d93def520574732f224fad98b
Omid <omid.haqbin@gmail.com>
2020-11-02 15:24:45 +0100

    polished js modal window

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 // modal Window 
 function formModal() {
     document.addEventListener('click', function (event) {
-        event = window.event;
-        const target = event.target ;
-        if (target.hasAttribute('data-form') && target.getAttribute('data-form') == 'modal') {
-            if (target.hasAttribute('data-target')) {
-                const m_ID = target.getAttribute('data-target');
+        event.preventDefault();
+        const target = event.target;
+        if (target.hasAttribute('data-form')) {
+            if (target.hasAttribute('data-window')) {
+                const m_ID = target.getAttribute('data-window');
                 document.getElementById(m_ID).classList.add('open');
-                event.preventDefault();
             }
         }
   
-        if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'modal') || target.classList.contains('modal')) {
+        if (target.hasAttribute('data-dismiss')) {
             const modal = document.querySelector('[class="modal open"]');
             modal.classList.remove('open');
-            event.preventDefault();
         }
-    }, false);
+    });
   }
-  
+
 formModal();
 
 
